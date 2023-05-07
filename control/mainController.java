ed5e614809351ed83754f2c518d6161bd8f4059f
Breno Bering <bbering88@gmail.com>
2023-05-07 16:45:21 -0300

    Adicionados recursos de GUI e metodos na classe de controle

--- control/mainController.java
+++ control/mainController.java
@@ -1,35 +1,114 @@
 package control;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.concurrent.Semaphore;
 
-public class mainController {
-    private int nPhilosophers = 5;
-    private int left;
-    private int right;
-    private int thinking = 0;
-    private int hunger = 1;
-    private int eating = 2;
-    int states[] = new int[nPhilosophers];
-    Semaphore mutexSemaphore = new Semaphore(1);
-    Semaphore arraySemaphore[] = new Semaphore[nPhilosophers];
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import thread.Philosopher;
 
-    public void setLeft(int i) {
-        this.left = (i-1)%nPhilosophers;
+public class mainController implements Initializable{
+
+  // DECLARACAO DOS RECURSOS DO FXML
+
+  @FXML
+  private ImageView fork01;
+
+  @FXML
+  private ImageView fork02;
+
+  @FXML
+  private ImageView fork03;
+
+  @FXML
+  private ImageView fork04;
+
+  @FXML
+  private ImageView fork07;
+
+  @FXML
+  private ImageView fork08;
+
+  @FXML
+  private ImageView thinkingButters;
+
+  @FXML
+  private ImageView thinkingEric;
+
+  @FXML
+  private ImageView thinkingKenny;
+
+  @FXML
+  private ImageView thinkingKyle;
+
+  @FXML
+  private ImageView thinkingStan;
+
+  // THREADS UTILIZADA NO CODIGO
+  
+  private Philosopher philosopher1;
+
+  private Philosopher philosopher2;
+
+  private Philosopher philosopher3;
+
+  private Philosopher philosopher4;
+
+  private Philosopher philosopher5;
+  
+  private int nPhilosophers = 5;
+  int state[] = new int[nPhilosophers];
+  Semaphore mutexSemaphore = new Semaphore(1);
+  Semaphore arraySemaphore[] = new Semaphore[nPhilosophers];
+
+  public void setEatingImg(int id){
+    if(id == 0){
+      thinkingEric.setImage(new Image("/assets/eric.png"));
     }
+  }
 
-    public void setRight(int i) {
-        this.right = (i+1)%nPhilosophers;
-    }
+  public void grabAFork(int id) throws InterruptedException {
+    mutexSemaphore.acquire();
+    state[id] = 1; // ESTADO DE FOME
+    testAFork(id);
+    mutexSemaphore.release();
+    arraySemaphore[id].acquire();
+  } // FIM DO METODO QUE PEGA OS GARFOS
 
-    public int getLeft() {
-        return left;
-    }
+  public void returnFork(int id) throws InterruptedException {
+    mutexSemaphore.acquire();
+    state[id] = 1; // ESTADO DE PENSANDO
+    testAFork((id-1)%5); // TESTANDO GARFO DA ESQUERDA
+    testAFork((id+1)%5); // TESTANDO GARFO DA DIREITA
+    mutexSemaphore.release();
+  } // FIM DEVOLVE GARFOS
 
-    public int getRight() {
-        return right;
-    }
+  public void testAFork(int id) throws InterruptedException {
+    if (state[id] == 1 && state[(id - 1) % 5] != 2 && state[(id + 1) % 5] != 2) {
+      state[id] = 2;
+      arraySemaphore[id].acquire();
+    } // FIM DO IF
+  } // FIM DO METODO QUE TESTA OS GARFOS
 
-    public void grabAFork(int id){
-
-    }
-}
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    this.philosopher1 = new Philosopher("butters", 0);
+    philosopher1.setController(this);
+    this.philosopher2 = new Philosopher("kyle", 1);
+    philosopher2.setController(this);
+    this.philosopher3 = new Philosopher("eric", 2);
+    philosopher3.setController(this);
+    this.philosopher4 = new Philosopher("stan", 3);
+    philosopher4.setController(this);
+    this.philosopher5 = new Philosopher("kenny", 4);
+    philosopher5.setController(this);
+    //philosopher1.run();
+    //philosopher2.run();
+    //philosopher3.run();
+    //philosopher4.run();
+    //philosopher5.run();
+  }
+} 
--- thread/Philosopher.java
+++ thread/Philosopher.java
@@ -1,5 +1,31 @@
 package thread;
 
-public class Philosopher {
-    
+import control.*;
+import javafx.application.Platform;
+
+public class Philosopher extends Thread {
+    private int id;
+    private String name;
+    mainController mainController = new mainController();
+
+    public void setController(mainController controller) {
+        this.mainController = controller;
+    }
+
+    public Philosopher(String name, int id) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            mainController.grabAFork(3);
+        } catch (InterruptedException e) {
+        }
+        try {
+            mainController.returnFork(3);
+        } catch (InterruptedException e) {
+        }
+    }
 }
